083274059918ecaa592e81a39041b98a997d4697
EC2 Default User <ec2-user@ip-172-31-80-172.ec2.internal>
2022-12-14 03:21:19 +0000

    Revamping code for read player

--- player_lambda/runtime/read_player.py
+++ player_lambda/runtime/read_player.py
@@ -2,18 +2,23 @@ import json
 import datetime
 import boto3
 
-from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
+from aws_lambda_powertools.logging import correlation_paths
 
 logger = Logger(service="ReadPlayer")
+tracer = Tracer()
+app = APIGatewayHttpResolver()
 
 
 dynamodb = boto3.resource('dynamodb')
 
 
-@logger.inject_lambda_context
-def handler(event: dict, context: LambdaContext) -> str:
-    logger.info({'request': event})
+@app.get("/api/player")
+@tracer.capture_method
+def get_todos():
+    #logger.info({'request': event})
     logger.info('ReadPlayer kicked off from Player EventBridge in Compute')
 
     table = dynamodb.Table(
@@ -23,8 +28,19 @@ def handler(event: dict, context: LambdaContext) -> str:
 
     data = response['Items']
 
+    logger.info("Testing")
+
     while 'LastEvaluatedKey' in response:
         response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
         data.extend(response['Items'])
 
-    return data
+    logger.info("Testing...")
+
+    return {"testing": "one"}
+
+
+# You can continue to use other utilities just as before
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_HTTP)
+@tracer.capture_lambda_handler
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    return app.resolve(event, context)
